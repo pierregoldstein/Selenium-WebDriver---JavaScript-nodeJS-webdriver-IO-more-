1b1330686d0fa7f7496e2662c2fb18401991c866
Pierre Goldstein <pierre.goldstein@gmail.com>
2019-03-21 22:16:51 -0700

    Section 6 notes

---
+++ tests/WebDriverIOUniversity.js
@@ -0,0 +1,22 @@
+describe("Verify webdriveruniveristy links on homepage work correctly", function(){
+    it("check that the Contact us button opens the Contact Us page", function(done){
+        browser.setViewportSize({
+    	        width: 1200,
+    	        height: 800
+            })  
+        browser.url('http://www.webdriveruniversity.com/')
+        var title = browser.getTitle()
+        console.log('Title is: ' + title);
+            
+        browser.click('#contact-us')
+        browser.pause(3000)
+    });
+
+    it("check that the Login button opens the Login Portal page", function(){
+        browser.url('http://www.webdriveruniversity.com/')
+        browser.click('#login-portal')
+        var title = browser.getTitle()
+        console.log('Title is: '+ title);
+    });
+});
+
