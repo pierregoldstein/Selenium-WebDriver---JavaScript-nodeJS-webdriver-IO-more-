04bb0329b98ade8e4edb2f995a5950634038b572
Pierre Goldstein <pierre.goldstein@gmail.com>
2019-03-23 19:04:54 -0700

    Section 10 notes

--- tests/WebDriverIOUniversity.js
+++ tests/WebDriverIOUniversity.js
@@ -1,9 +1,14 @@
+var assert = require('assert');
+
 describe("Verify webdriveruniveristy links on homepage work correctly", function(){
     it("check that the Contact us button opens the Contact Us page", function(done){
         browser.setViewportSize({
     	        width: 1200,
     	        height: 800
             })  
+        browser.url('/');
+        var title = browser.getTitle();
+        assert.equal(title, 'WebDriverUniversity.com')
         var title = browser.getTitle()
         console.log('Title is: ' + title);
             
@@ -12,6 +17,9 @@ describe("Verify webdriveruniveristy links on homepage work correctly", function
     });
 
     it("check that the Login button opens the Login Portal page", function(){
+        browser.url('/');
+        var title = browser.getTitle();
+        assert.equal(title, 'WebDriverUniversity.com')
         browser.click('#login-portal')
         var title = browser.getTitle()
         console.log('Title is: '+ title);
