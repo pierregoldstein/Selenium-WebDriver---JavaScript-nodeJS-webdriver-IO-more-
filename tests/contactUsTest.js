b31d274f2fcf4e0bb74528556f0436bdd12cace0
Pierre Goldstein <pierre.goldstein@gmail.com>
2019-04-21 15:02:15 -0700

    Section 18 notes

--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -1,46 +1,55 @@
-beforeEach(function(){
-    browser.url('/Contact-Us/contactus.html' );
+var request = require('sync-request'); //Load the sync request library
+
+beforeEach(function () {
+    browser.url('/Contact-Us/contactus.html');
 });
 
-describe('Test Contact Us form on Webdriveruniveristy.com', function(){
-    beforeEach(function(){
+describe('Test Contact Us form on Webdriveruniveristy.com', function () {
+    var res = request('GET', 'http://jsonplaceholder.typicode.com/posts/1/comments'); //Create the variable to "GET" json data
+
+    var contacUsDetails = JSON.parse(res.getBody().toString('utf8')); //Format the JSON data 
+
+    beforeEach(function () {
         console.log('We are inside the describe block');
     });
-    it('Should be able to submit a sucessful submission via contact us form', function(done){
-        browser.setValue("[name='first_name']", 'Pierre');
-        browser.setValue("[name='last_name']", 'Goldstein');
-        browser.setValue("[name='email']", 'pierre.goldstein@gmail.com');
-        browser.setValue("[name='message']",'Bizzaro I\'m helping');
-        browser.click("[type='submit']");
-        
-        var sucessfulContactConfirmation = browser.isExisting("#contact_reply h1");
-        expect(sucessfulContactConfirmation, "Sucessful submission Message does not exist").to.be.true;
+    // contacUsDetails.forEach(function (contacUsDetails) {
+    contacUsDetails.slice(0,5).forEach(function (contacUsDetails){ //Using the slice method to limit the number of request because the file is ginormous!!!
+        it.only('Should be able to submit a sucessful submission via contact us form', function (done) { //it.only is used just to run this one test
+            browser.setValue("[name='first_name']", 'Pierre');
+            browser.setValue("[name='last_name']", 'Goldstein');
+            browser.setValue("[name='email']", contacUsDetails.email); //Updated info with parsed json data for email
+            browser.setValue("[name='message']", contacUsDetails.body); //Updated info with parsed json data for body
+            browser.click("[type='submit']");
 
-        var sucessfulSubmission = browser.getText("#contact_reply h1");
-        expect(sucessfulSubmission).to.equal('Thank You for your Message!');
+            var sucessfulContactConfirmation = browser.isExisting("#contact_reply h1");
+            expect(sucessfulContactConfirmation, "Sucessful submission Message does not exist").to.be.true;
+
+            var sucessfulSubmission = browser.getText("#contact_reply h1");
+            expect(sucessfulSubmission).to.equal('Thank You for your Message!');
+        })
     });
-    it('Should not be able to submit a sucessful submission via contact us form as all fields are required', function(done){
+    it('Should not be able to submit a sucessful submission via contact us form as all fields are required', function (done) {
         browser.setValue("[name='first_name']", 'Pierre');
         browser.setValue("[name='last_name']", 'Goldstein');
         browser.setValue("[name='email']", 'pierre.goldstein@gmail.com');
         browser.click("[type='submit']");
-       
+
         var sucessfulContactConfirmation = browser.isExisting("#contact_reply h1");
         expect(sucessfulContactConfirmation, "Sucessful submission Message does not exist").to.be.false;
     });
-    it('Should not be able to submit a sucessful submission via contact us form as all fields are required', function(done){
+    it('Should not be able to submit a sucessful submission via contact us form as all fields are required', function (done) {
         browser.setValue("[name='first_name']", 'Pierre');
         browser.setValue("[name='email']", 'pierre.goldstein@gmail.com');
         browser.click("[type='submit']");
-        
+
         var sucessfulContactConfirmation = browser.isExisting("#contact_reply h1");
         expect(sucessfulContactConfirmation, "Sucessful submission Message does not exist").to.be.false;
     });
-    it('Should not be able to submit a sucessful submission via contact us form as all fields are required', function(done){
+    it('Should not be able to submit a sucessful submission via contact us form as all fields are required', function (done) {
         browser.setValue("[name='first_name']", 'Pierre');
         browser.setValue("[name='last_name']", 'Goldstein');
         browser.click("[type='submit']");
-        
+
         var errorText = browser.getText('body');
         expect(errorText).to.include('Error: all fields are required');
 
