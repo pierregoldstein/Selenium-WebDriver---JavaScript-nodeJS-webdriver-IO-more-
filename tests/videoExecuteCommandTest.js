72f1a55c481a118bf4646a495ab8cc03ba28a54b
Pierre Goldstein <pierre.goldstein@gmail.com>
2019-04-21 18:33:46 -0700

    Section 20 notes

---
+++ tests/videoExecuteCommandTest.js
@@ -0,0 +1,31 @@
+beforeEach(function() {
+	browser.url("https://www.w3schools.com/html/html5_video.asp");
+})
+
+//Injects a snippet of JavaScript into the page for execution in the context of the currently selected frame.
+browser.addCommand('isVideoPaused', function(){
+	var isPaused = browser.execute(function(){
+		console.log('Outputted inside the console window');
+		//id locator of the video on the page
+		var video = document.querySelector('#video1');
+		return video.paused;
+	});
+	return isPaused.value;
+});
+
+describe('Video test', function() {
+	it('Validate that the video is paused when accessing the page', function(done) {
+		this.timeout(20000);
+		var isPaused = browser.isVideoPaused();
+		expect(isPaused).to.be.true;
+		browser.pause(3000);
+	});
+
+	it('Alter the width of the video', function(done) {
+		var videoWidth = browser.execute(function(){
+			var video = document.querySelector('#video1');
+			return video.style.width = "300px";
+		});
+		browser.pause(3000);
+	});
+});
