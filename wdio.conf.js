07912b7cda86fb68c75e0508dbd2f1ccdeda5c74
Pierre Goldstein <pierre.goldstein@gmail.com>
2019-05-02 15:12:27 -0700

    Section 24 notes Part 2

--- wdio.conf.js
+++ wdio.conf.js
@@ -133,21 +133,23 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: http://webdriver.io/guide/reporters/dot.html
-    reporters: ['dot', 'junit', 'json', 'allure'],
+    reporters: ['dot', /*'junit', 'json',*/ 'allure'],
 
     reporterOptions: {
+        /* 
         junit: {
             outputDir: './reports/junit-results/'
         },
         json: {
             outputDir: './reports/json-results/'
         },
+        */
         allure: {
             outputDir: './reports/allure-results/',
             disableWebdriverStepsReporting: true,
             disableWebdriverScreenshotsReporting: false,
             useCucumberStepReporter: false
-        },
+        }
     },
     //
     // Options to be passed to Mocha.
@@ -253,10 +255,10 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that ran
      */
-    after: function (result, capabilities, specs) {
-        var name = 'ERROR-chrome-'+Date.now();
-        browser.saveScreenshot('./errorShots/'+name+'.png');
-    },
+    // after: function (result, capabilities, specs) {
+    //     var name = 'ERROR-chrome-'+Date.now();
+    //     browser.saveScreenshot('./errorShots/'+name+'.png');
+    // },
     /**
      * Gets executed right after terminating the webdriver session.
      * @param {Object} config wdio configuration object
